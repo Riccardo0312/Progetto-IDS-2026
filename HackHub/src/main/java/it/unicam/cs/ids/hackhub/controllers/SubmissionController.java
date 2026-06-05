a17474f0d86a7a6f55b89ca705a4903bf1378986
AngeloAlbanesi <angeloalbanesi99@gmail.com>
2026-06-05 17:43:42 +0200

    feat: aggiungi autorizzazione per gli utenti nel SubmissionController

--- HackHub/src/main/java/it/unicam/cs/ids/hackhub/controllers/SubmissionController.java
+++ HackHub/src/main/java/it/unicam/cs/ids/hackhub/controllers/SubmissionController.java
@@ -8,6 +8,7 @@ import it.unicam.cs.ids.hackhub.service.interfaces.ISubmissionService;
 import it.unicam.cs.ids.hackhub.service.mapper.SubmissionMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api")
+@PreAuthorize("hasRole('USER')")
 public class SubmissionController {
 
 	private final ISubmissionService submissionService;
