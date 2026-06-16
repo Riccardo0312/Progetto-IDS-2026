3287980ab2b86ad3652ecb03a3b5dfef5d06e0b7
alessia002 <alessia8.bussi8@gmail.com>
2026-06-16 10:18:15 +0200

    - Calcolare punteggio finale - Generare classifica finale - Visualizza classifica finale

--- HackHub/src/main/java/it/unicam/cs/ids/hackhub/dto/leaderboard/LeaderboardEntryDTO.java
+++ HackHub/src/main/java/it/unicam/cs/ids/hackhub/dto/leaderboard/LeaderboardEntryDTO.java
@@ -1,11 +1,11 @@
 package it.unicam.cs.ids.hackhub.dto.leaderboard;
 
-public class LeaderboardEntryDTO {
-    public record LeaderboardEntryDTO(
+
+  public record LeaderboardEntryDTO(
             int position,
             Long teamId,
             String teamName,
             Long submissionId,
             String submissionTitle,
             int score) {}
-}
+
--- HackHub/src/main/java/it/unicam/cs/ids/hackhub/dto/leaderboard/LeaderboardResponseDTO.java
+++ HackHub/src/main/java/it/unicam/cs/ids/hackhub/dto/leaderboard/LeaderboardResponseDTO.java
@@ -1,11 +1,9 @@
 package it.unicam.cs.ids.hackhub.dto.leaderboard;
 import java.util.List;
 
-
-public class LeaderboardResponseDTO {
-    public record LeaderboardResponseDTO(
+ public record LeaderboardResponseDTO(
             Long hackathonId,
             String hackathonName,
             List<LeaderboardEntryDTO> entries) {}
 
-}
+
